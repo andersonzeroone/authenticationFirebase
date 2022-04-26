d5ee1c397642313ba9aca90a3ecc592885f5554d
Anderson Pablo <anderson.pablo02@gmail.com>
2022-04-25 21:31:31 -0300

    feat(screen - welcomescreen): accessing bank private data in firebase

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,31 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://expensereactnative-default-rtdb.firebaseio.com/messege.json?auth=' +
+        token
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
